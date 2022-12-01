725ccca3c7362f11af9a5dbf8e962006d4ca06ea
ahub <ahub@riseup.net>
2022-12-01 17:00:56 +0100

    Add template for day2

---
+++ AdventOfcode/Puzzle2.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+
+namespace AdventOfcode;
+
+// ReSharper disable once UnusedType.Global
+public class Puzzle2 : PuzzleSolver
+{
+    public Puzzle2(string fileContent) : base(fileContent)
+    {
+    }
+
+    protected override string Step1(string fileContent)
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string Step2(string fileContent)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static class Test2
+    {
+        [Test]
+        public static void PuzzleTest()
+        {
+        }
+    }
+}
